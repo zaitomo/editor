f752ccf6c983b048e6b76c3d3ca8c413c3029773
zaitomo <zaitomo41@gmail.com>
2016-09-23 15:28:34 +0900

    init.el

--- emacs/init.el
+++ emacs/init.el
@@ -71,28 +71,49 @@
 
 (add-hook 'haskell-mode-hook (lambda () (flymake-mode)))
 (add-hook 'haskell-mode-hook '(lambda ()
-								(ghc-init)
-								(local-set-key "\C-j" (lambda () (interactive)(insert " -> ")))
-								(local-set-key "\M-j" (lambda () (interactive)(insert " => ")))
-								(local-set-key "\C-l" (lambda ()(interactive)(insert " <- ")))
-								))
+(ghc-init)
+        (local-set-key "\C-j" (lambda () (interactive)(insert " -> ")))
+        (local-set-key "\M-j" (lambda () (interactive)(insert " => ")))
+        (local-set-key "\C-l" (lambda ()(interactive)(insert " <- ")))
+            ))
 
+(add-hook 'haskell-mode-hook 'inf-haskell-mode) ;; enable
 (defadvice inferior-haskell-load-file (after change-focus-after-load)
   "Change focus to GHCi window after C-c C-l command"
   (other-window 1))
 (ad-activate 'inferior-haskell-load-file)
 
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+
 (custom-set-variables
- '(haskell-indent-offset 4)
- '(haskell-indent-spaces 4)
- )
+ '(haskell-indent-after-keywords (quote (("where" 2 0) ("of" 2) ("do" 2) ("mdo" 2) ("rec" 2) ("in" 2 0) ("{" 2) "if" "then" "else" "let")))
+ '(haskell-indent-offset 2)
+ '(haskell-indent-spaces 2))
 
-;; auto-complete
+;; ghc-mod
 (require 'auto-complete)
-(require 'auto-complete-config)
-
-(require 'ac-haskell-process)
+(ac-define-source ghc-mod
+  '((depends ghc)
+    (candidates . (ghc-select-completion-symbol))
+    (symbol . "s")
+    (cache)))
+(require 'ac-haskell-process) ; if not installed via package.el
 (add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
 (add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
-(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'haskell-interactive-mode))
-(put 'downcase-region 'disabled nil)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'haskell-interactive-mode))
+;; ;; https://github.com/m2ym/auto-complete
+(defun my-ac-haskell-mode ()
+  (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod)))
+(add-hook 'haskell-mode-hook 'my-ac-haskell-mode)
+
+(defun my-haskell-ac-init ()
+  (when (member (file-name-extension buffer-file-name) '("hs" "lhs"))
+    (auto-complete-mode t)
+    (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod))))
+
+(add-hook 'find-file-hook 'my-haskell-ac-init)
+(put 'set-goal-column 'disabled nil)
